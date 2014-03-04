4bdddddf923efeec623b0ced428077e01f84d817
Taras101 <taras101@gmail.com>
2014-03-03 21:55:51 -0500

    added stores model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140304021048) do
+ActiveRecord::Schema.define(version: 20140304025423) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,12 @@ ActiveRecord::Schema.define(version: 20140304021048) do
     t.string   "name"
     t.string   "desc"
     t.string   "notes"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "stores", force: true do |t|
+    t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
