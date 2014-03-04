9908d921b5df5a8d21b53e8373fe4d9703ebd511
Taras101 <taras101@gmail.com>
2014-03-03 20:50:30 -0500

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Grocerygogo::Application.config.secret_key_base = '1e045efcec67854fa828027dc7428eca0f636e38dd8fa9e7ec46f3bee5277965d5e6359ddc7475da715c1e71519d9c90c48946791bdc642639bf9d08d22e66d5'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Grocerygogo::Application.config.session_store :cookie_store, key: '_grocerygogo_session'
