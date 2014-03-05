dab536b5322cd76dbc4e1e8002bf013502fb2c2f
Tyler Hackbart <tylerhackbart@Tylers-MacBook-Air.local>
2014-03-05 17:05:24 -0500

    Adding sorcery and User

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Grocerygogo::Application.routes.draw do
+  get "logout" => "sessions#destroy", :as => "logout"
+  get "login" => "sessions#new", :as => "login"
+  get "signup" => "users#new", :as => "signup"
+  resources :users
   resources :stores
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
