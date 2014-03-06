d49baf293121819f66c17f451580475fcc77784e
Taras101 <taras101@gmail.com>
2014-03-06 18:21:44 -0500

    added valid to form and fixed price to cents

--- app/models/price.rb
+++ app/models/price.rb
@@ -1,5 +1,11 @@
 class Price < ActiveRecord::Base
 	belongs_to :store
 	belongs_to :product
-		validates :price, :measure, :amount ,presence: true
+	validates :price, :measure, :amount ,presence: true
+	scope :current, -> {where("valid_from < ? and valid_to > ?", Time.now, Time.now)}
+
+	def formatted_price
+    price_in_dollars = price.to_f / 100
+    sprintf("%.2f", price_in_dollars)
+  end
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -2,7 +2,5 @@ class Store < ActiveRecord::Base
 	has_many :products, through: :prices
 	has_many :prices
 
-	def self.available_stores
-	end
 
 end
