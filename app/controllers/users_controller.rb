f8d9791a503f69aa8a49b7f91c9d7d0a28b4a1af
Tyler Hackbart <tylerhackbart@Tylers-MacBook-Air.local>
2014-03-06 18:20:20 -0500

    User Works

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+    def index
+    @user = current_user
+  end
+
+  def show
+   @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def create
+    @users = User.new(user_params)
+
+      if @users.save
+        redirect_to products_url, :notice => "Signed Up!"
+      else
+        render :new
+      end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
+
+private
+  def user_params
+    params.require(:user).permit(:email, :password, :salt)
+  end
+
