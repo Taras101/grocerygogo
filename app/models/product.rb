7f1faf6aff6f1a9f139c49661a2b138b9984d02d
Taras101 <taras101@gmail.com>
2014-03-06 16:25:36 -0500

    added stores drop down and display products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,6 @@
 class Product < ActiveRecord::Base
 	has_many :prices
-	accepts_nested_attributes_for :prices
 	has_many :stores, through: :prices
-
+	accepts_nested_attributes_for :prices, :stores
 	validates :name, :brand, :price, :measure, :amount,presence: true
 end
