164112022a483fcfc55f5c1cd1eea99078e64c2a
Taras101 <taras101@gmail.com>
2014-03-10 17:13:50 -0400

    list search working

--- app/models/product.rb
+++ app/models/product.rb
@@ -10,21 +10,25 @@ class Product < ActiveRecord::Base
 
 	  unless search.blank?
 	  	if store
+	  		puts "aaaaa"
 		    Price.current
 		    			.includes([:product, :store])
 		    			.where('LOWER(products.name) LIKE ?', "%#{search.downcase}%")
 		    			.where(:store => store)
 		    			.references(:products)
 	  	else
+	  		puts 'bbbbbbbb'
 		    Price.current.includes([:product])
 		    						 .where('LOWER(products.name) LIKE ?', "%#{search.downcase}%")
 		    						 .references([:products])
 	  	end
 	  else
 	  	if store
+	  		puts "cccccccccc"
 		    Price.current.includes([:product, :store])
 		    						 .where(:store => store)
 	  	else
+	  		puts "ddddddddddd"
 		    Price.current.includes(:product)
 	  	end
 	  end
