d90fe15efa8beedfce49214d5e253295410ae491
Taras101 <taras101@gmail.com>
2014-03-13 15:25:23 -0400

    ready fir serialize

--- app/models/grocery_item.rb
+++ app/models/grocery_item.rb
@@ -1,6 +1,7 @@
 class GroceryItem < ActiveRecord::Base
 	belongs_to :user
 	acts_as_list scope: :user
+	default_scope :order => :position
 	def self.search(search)
 	  if search
 	    find(:all, :conditions => ['LOWER(name) LIKE ?', "%#{search.downcase}%"])
